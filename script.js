de12a079d35b72a3efcf0da11f35f5e495aa5d2a
Konrad Bauer <konradbauer94@gmail.com>
2023-05-17 00:03:07 +0200

    implement add task feature, input reset and autofocus when task is added

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
 {
-  const tasks = [
-    {
-      content: "L",
-      done: false,
-    },
-    {
-      content: "Lorem ipsum dolor sit ametaaaaaaaaaaa",
-      done: false,
-    },
-    {
-      content: "Lorem ipsum dolor sit ametaaaaaaaaaaa",
-      done: false,
-    },
-  ];
+  const tasks = [];
 
   const render = () => {
     let contentString = "";
 
     for (const task of tasks) {
       contentString += `
-        <li class="mainContainer__li">
-        <span class="ul__checkbox"></span>${task.content}</li>
+        <li class="mainContainer__li 
+        ${task.done ? "mainContainer__li--done" : ""}"
+        >
+        <span class="ul__checkbox"></span>
+        ${task.content}
+        </li>
         `;
     }
 
     document.querySelector(".js-list").innerHTML = contentString;
   };
 
+  const addTask = (newTaskContent) => {
+    tasks.push({ content: newTaskContent });
+
+    render();
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+    if (newTaskContent === "") {
+      return;
+    }
+
+    addTask(newTaskContent);
+
+    document.querySelector(".js-newTask").focus();
+    document.querySelector(".js-form").reset();
+  };
+
   const init = () => {
     render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
   };
 
   init();
